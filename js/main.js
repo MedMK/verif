6581d05db7fbf7cc79ff64ada957bccb412bacac
MedMK <96750450+MedMK@users.noreply.github.com>
2022-06-07 23:05:13 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+(function() {
+    // K'ED BY CHOWGANG
+
+    window.onload = function() {
+        window.setTimeout(fadeout, 500);
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+        $("#modal1").modal("show");
+    }
+
+    $("#modal2").on('show.bs.modal', function(e) {
+        $("#modal1").modal("hide");
+    });
+
+    $("#closeBtn").on('click', function(e) {
+        $(".info").fadeIn(800);
+        $("#modal2").modal("hide");
+        $('div').removeClass("blurit");
+    });
+    $("#modal2").on('click', function(e) {
+        $('div').removeClass("blurit");
+        $(".info").fadeIn(800);
+        $("#modal2").modal("hide");
+    });
+
+
+})();
+
+function modal2Closed() {
+    console.log("CHOWWWWWWWWW GUYS");
+    $("#modal2").modal("hide");
+}
